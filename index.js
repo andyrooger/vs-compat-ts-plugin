c33bf8f6d7ab80d3608756542ec40d80975e611e
Andy Gurden <as.gurden@gmail.com>
2020-04-10 03:44:45 +0100

    Set cwd when provided in the plugin

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const PLUGIN_NAME = 'vs-compat-ts-plugin';
 
 function init(modules) {
 
+    function setCwd(cwd, log) {
+        if(cwd) {
+            process.chdir(cwd);
+            log(`Updating cwd to ${cwd}`)
+        }
+    }
+
     function create(info) {
         const log = msg => info.project.projectService.logger.info(`[${PLUGIN_NAME}] ${msg}`);
         log('Loaded plugin');
+
+        setCwd(info.config.workingDirectory, log);
 
         return info.languageService;
     }
