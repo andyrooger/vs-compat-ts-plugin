279cba0533c4fd2a187f9ef833cc155b3343a6c3
Andy Gurden <as.gurden@gmail.com>
2020-04-15 01:05:08 +0100

    test: project dir fixture
    Add the generated project dir to fixtures so it can be shared easier in the simplified tests

--- test/cwd.spec.js
+++ test/cwd.spec.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const fs = require('fs');
-const tape = require('tape');
-const { setupTemp, cleanupTemp, loadAndRunPlugins, pluginTest } = require('./tempProject');
-const { THIS_PLUGIN, LOG_CWD_PLUGIN, LOG_LOADTIME_CWD_PLUGIN, EMPTY_DIRS } = require('./fixtures');
+const { pluginTest } = require('./tempProject');
+const { THIS_PLUGIN, LOG_CWD_PLUGIN, LOG_LOADTIME_CWD_PLUGIN, EMPTY_DIRS, PROJECT_DIR } = require('./fixtures');
 
 const [SERVER_CWD, PLUGIN_CWD] = EMPTY_DIRS;
+const RELATIVE_PLUGIN_CWD = path.relative(PROJECT_DIR, PLUGIN_CWD);
 
 pluginTest('cwd is set before the next plugin init', {
     serverCwd: SERVER_CWD,
@@ -52,38 +51,24 @@ pluginTest('invalid cwd does not break everything else', {
     }
 });
 
-tape('cwd relative to the tsconfig directory', t => {
-    const serverCwd = path.resolve(__dirname, '..');
-    const pluginCwd = path.join('.', 'workingDir');
-
-    const tempDir = setupTemp();
-    const fullPluginCwd = path.resolve(tempDir, pluginCwd);
-    fs.mkdirSync(fullPluginCwd);
-    const plugins = [
-        { name: THIS_PLUGIN.path, workingDirectory: pluginCwd },
+pluginTest('cwd relative to the tsconfig directory', {
+    serverCwd: SERVER_CWD,
+    plugins: [
+        { name: THIS_PLUGIN.path, workingDirectory: RELATIVE_PLUGIN_CWD },
         { name: LOG_CWD_PLUGIN.path }
-    ];
-    loadAndRunPlugins({ plugins, serverCwd }).then(({ hasMessageBy }) => {
-        t.ok(hasMessageBy(LOG_CWD_PLUGIN.name, fullPluginCwd));
-        cleanupTemp();
-        t.end();
-    });
+    ],
+    check: (t, { hasMessageBy }) => {
+        t.ok(hasMessageBy(LOG_CWD_PLUGIN.name, PLUGIN_CWD));
+    }
 });
 
-tape('cwd can use forward slashes (in all OS)', t => {
-    const serverCwd = path.resolve(__dirname, '..');
-    const pluginCwd = './workingDirectory';
-
-    const tempDir = setupTemp();
-    const fullPluginCwd = path.resolve(tempDir, pluginCwd);
-    fs.mkdirSync(fullPluginCwd);
-    const plugins = [
-        { name: THIS_PLUGIN.path, workingDirectory: pluginCwd },
+pluginTest('cwd can use forward slashes (in all OS)', {
+    serverCwd: SERVER_CWD,
+    plugins: [
+        { name: THIS_PLUGIN.path, workingDirectory: RELATIVE_PLUGIN_CWD.replace(/\\/g, '/') },
         { name: LOG_CWD_PLUGIN.path }
-    ];
-    loadAndRunPlugins({ plugins, serverCwd }).then(({ hasMessageBy }) => {
-        t.ok(hasMessageBy(LOG_CWD_PLUGIN.name, fullPluginCwd));
-        cleanupTemp();
-        t.end();
-    });
+    ],
+    check: (t, { hasMessageBy }) => {
+        t.ok(hasMessageBy(LOG_CWD_PLUGIN.name, PLUGIN_CWD));
+    }
 });
