de88391bd268f014060a2b4f4488989650a861da
Andy Gurden <as.gurden@gmail.com>
2020-04-16 01:52:50 +0100

    test: broad ts versions
    Make sure we're testing on all the versions of TS that I can get the tests to pass on. This means
    >=2.9

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -22,12 +22,21 @@ module.exports = {
         name: 'breakMockRequirePlugin', path: path.resolve(__dirname, 'plugins', 'breakMockRequirePlugin.js')
     },
     TS_VERSIONS: { 
+        // Latest and oldest of each major release
         '3.8': { version: '3.8.3', path: path.resolve(__dirname, '..', '..', 'node_modules', 'typescript38') },
+        '3.0': { version: '3.0.1', path: path.resolve(__dirname, '..', '..', 'node_modules', 'typescript30') },
+        '2.9': { version: '2.9.1', path: path.resolve(__dirname, '..', '..', 'node_modules', 'typescript29') },
+         // Can't go back further than 2.8 because tsserver won't work on node 10 (https://github.com/microsoft/TypeScript/commit/1fc3aebc20a13048d6595395f721c6b0078c2c08)
+         // Can't go back further than 2.9 because the tsconfig warnings stop appearing
+
+        // Has to be a different version to all the others for the tsversion tests
         'default': { version: '3.7.5', path: path.resolve(__dirname, '..', '..', 'node_modules', 'typescript') }
     },
     EMPTY_DIRS: [
         path.resolve(__dirname, 'dirs', 'dir1'),
         path.resolve(__dirname, 'dirs', 'dir2')
     ],
-    PROJECT_DIR: path.resolve(__dirname, 'workspace')
+    PROJECT_DIR: path.resolve(__dirname, 'workspace'),
+    PROJECT_FILE: path.resolve(__dirname, 'workspace', 'tsconfig.json'),
+    PROJECT_CONTENT: path.resolve(__dirname, 'workspace', 'file.ts')
 };
--- test/tsversion.spec.js
+++ test/tsversion.spec.js
@@ -2,51 +2,49 @@ const tape = require('tape');
 const { pluginTest } = require('./helpers');
 const { THIS_PLUGIN, LOG_TS_VERSIONS_PLUGIN, LOG_TOP_TS_VERSIONS_PLUGIN, BREAK_MOCK_REQUIRE_PLUGIN, TS_VERSIONS } = require('./fixtures');
 
-const serverTs = TS_VERSIONS['3.8'];
-
 tape('typescript standard package must be different version than the one tsserver is running from', t => {
-    t.notEqual(serverTs.version, TS_VERSIONS.default.version);
+    for(const tsVersionKey of Object.keys(TS_VERSIONS)) {
+        if(tsVersionKey === 'default')
+            continue;
+        t.notEqual(TS_VERSIONS[tsVersionKey].version, TS_VERSIONS.default.version);
+    }
     t.end();
 });
 
 pluginTest('typescript not mocked if useVSTypescript is false', {
-    typescriptServerDir: serverTs.path,
     plugins: [
         { name: THIS_PLUGIN.path, useVSTypescript: false },
         { name: LOG_TS_VERSIONS_PLUGIN.path }
     ],
-    check: (t, { hasMessageBy }) => {
+    check: (t, { hasMessageBy, tsVersion }) => {
         t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `required: ${TS_VERSIONS.default.version}`));
-        t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `plugin: ${serverTs.version}`));
+        t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `plugin: ${tsVersion.version}`));
     }
 });
 
 pluginTest('typescript is mocked if useVSTypescript is true', {
-    typescriptServerDir: serverTs.path,
     plugins: [
         { name: THIS_PLUGIN.path, useVSTypescript: true },
         { name: LOG_TS_VERSIONS_PLUGIN.path }
     ],
-    check: (t, { hasMessageBy }) => {
-        t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `required: ${serverTs.version}`));
-        t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `plugin: ${serverTs.version}`));
+    check: (t, { hasMessageBy, tsVersion }) => {
+        t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `required: ${tsVersion.version}`));
+        t.ok(hasMessageBy(LOG_TS_VERSIONS_PLUGIN.name, `plugin: ${tsVersion.version}`));
     }
 });
 
 pluginTest('typescript is mocked even at the top level of other plugins if useVSTypescript is true', {
-    typescriptServerDir: serverTs.path,
     plugins: [
         { name: THIS_PLUGIN.path, useVSTypescript: true },
         { name: LOG_TOP_TS_VERSIONS_PLUGIN.path }
     ],
-    check: (t, { hasMessageBy }) => {
-        t.ok(hasMessageBy(LOG_TOP_TS_VERSIONS_PLUGIN.name, `required: ${serverTs.version}`));
-        t.ok(hasMessageBy(LOG_TOP_TS_VERSIONS_PLUGIN.name, `plugin: ${serverTs.version}`));
+    check: (t, { hasMessageBy, tsVersion }) => {
+        t.ok(hasMessageBy(LOG_TOP_TS_VERSIONS_PLUGIN.name, `required: ${tsVersion.version}`));
+        t.ok(hasMessageBy(LOG_TOP_TS_VERSIONS_PLUGIN.name, `plugin: ${tsVersion.version}`));
     }
 });
 
 pluginTest('failed typescript mock does not break the plugin', {
-    typescriptServerDir: serverTs.path,
     plugins: [
         { name: BREAK_MOCK_REQUIRE_PLUGIN.path },
         { name: THIS_PLUGIN.path, useVSTypescript: true }
